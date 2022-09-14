caf838403fa5eb8f71d74b8c4bc672600039dd44
maregn <maregnbaykeda@gmail.com>
2022-09-14 10:58:23 -0700

    print last digitsee

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,9 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int last = n % 10;
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
 }
